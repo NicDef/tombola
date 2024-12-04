2dce26ed7e60bb9a34468b10012f1933767f3565
NicDef <nicolas.defee@gmx.de>
2024-12-04 18:09:52 +0100

    updated messages on web service

--- main.py
+++ main.py
@@ -12,6 +12,14 @@ with open("tombola_data.csv", "r") as f:
 def redeem():
     code = request.args.get("code")
     if code in tombola_dict:
+        result = str(tombola_dict[code])  # Hauptgewinn / Gewinn / Niete
+        if result == "Hauptgewinn":
+            return f"<h1>Herzlichen Glückwunsch zu einem <b>Hauptgewinn</b>! Bitte zeige deinen QR-Code am Stand der Tombola vor, um den Gewinn zu erhalten."
+        elif result == "Gewinn":
+            return f"<h1>Herzlichen Glückwunsch zu einem <b>Gewinn</b>! Bitte zeige deinen QR-Code am Stand der Tombola vor, um den Gewinn zu erhalten."
+        elif result == "Niete":
+            return f"<h1>Leider eine <b>Niete</B>. Aber nicht traurig sein, das gesammelte Geld der Lose wird für einen gemeinnützigen Zweck verwendet."
+
         return f"{tombola_dict[code]}"
     else:
         return "Ungültiger QR-Code. Melden Sie sich bitte beim Stand der Tombola"
